1f5508dba218d5a586aca39a874a030b6d74ac35
Philipp Paris <paris.philipp@gmail.com>
2022-12-22 15:40:33 +0100

    day11 - part2

--- src/main/java/aoc/day11/Day11.java
+++ src/main/java/aoc/day11/Day11.java
@@ -8,10 +8,19 @@ import java.util.stream.Collectors;
 public class Day11 {
 
     public long part1(final List<Monkey> input) {
+        return exec(input, i -> i / 3, 20);
+    }
+
+    public long part2(final List<Monkey> input) {
+        final var commonMultiple = input.stream().mapToLong(Monkey::getDivisor).reduce(1, (k, j) -> k * j);
+        return exec(input, i -> i % commonMultiple, 10000);
+    }
+
+    public long exec(final List<Monkey> input, final Function<Long, Long> reliefModifier, final int rounds) {
         final var monkeys = input.stream().collect(Collectors.toMap(Monkey::getId, Function.identity()));
-        for (var i = 0; i < 20; i++) {
+        for (var i = 0; i < rounds; i++) {
             for (var monkey : monkeys.values()) {
-                monkey.exec(monkeys);
+                monkey.exec(monkeys, reliefModifier);
             }
         }
         return monkeys.values().stream().map(Monkey::getInspectCounter).sorted(Comparator.reverseOrder()).limit(2)
--- src/main/java/aoc/day11/Monkey.java
+++ src/main/java/aoc/day11/Monkey.java
@@ -1,46 +1,45 @@
 package aoc.day11;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
-import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Monkey {
 
     private final int id;
-    private final List<Integer> items;
-    private final Function<Integer, Integer> operation;
-    private final Predicate<Integer> test;
+    private final List<Long> items;
+    private final Function<Long, Long> operation;
+    private final int divisor;
     private final int targetSuccess;
     private final int targetFailure;
 
-    private int inspectCounter = 0;
+    private long inspectCounter = 0;
 
-    public Monkey(int id, List<Integer> items, Function<Integer, Integer> operation, Predicate<Integer> test,
+    public Monkey(int id, List<Integer> items, Function<Long, Long> operation, int divisor,
                    int targetSuccess, int targetFailure) {
         this.id = id;
-        this.items = new ArrayList<>(items);
+        this.items = items.stream().map(i -> (long) i).collect(Collectors.toList());
         this.operation = operation;
-        this.test = test;
+        this.divisor = divisor;
         this.targetSuccess = targetSuccess;
         this.targetFailure = targetFailure;
     }
 
-    public void exec(final Map<Integer, Monkey> monkeys) {
+    public void exec(final Map<Integer, Monkey> monkeys, Function<Long, Long> reliefModifier) {
         for (var item : items) {
-            inspectCounter++;
-            var newItem = operation.apply(item) / 3;
-            if (test.test(newItem)) {
-                monkeys.get(targetSuccess).add(newItem);
+            final var value = reliefModifier.apply(operation.apply(item));
+            if (value % divisor == 0) {
+                monkeys.get(targetSuccess).add(value);
             } else {
-                monkeys.get(targetFailure).add(newItem);
+                monkeys.get(targetFailure).add(value);
             }
         }
+        inspectCounter += items.size();
         items.clear();
     }
 
-    public void add(final Integer item) {
+    public void add(final long item) {
         items.add(item);
     }
 
@@ -48,7 +47,11 @@ public class Monkey {
         return id;
     }
 
-    public int getInspectCounter() {
+    public long getInspectCounter() {
         return inspectCounter;
+    }
+
+    public int getDivisor() {
+        return divisor;
     }
 }
